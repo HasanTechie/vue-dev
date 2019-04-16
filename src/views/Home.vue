1f3ecf8c11c83b2d8a78316fe0a738ef841ab278
hasanabbax <hasanabbax@gmail.com>
2019-04-16 06:52:54 +0000

    added navigation toolbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an home page</h1>
+  <div class="home">
+    <h1 class="display-4">This is an home page</h1>
+    <v-btn>
+      <v-icon>email</v-icon>
+    </v-btn>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cumque dicta, dolore, doloribus eligendi excepturi hic incidunt iusto modi nemo nulla odio pariatur praesentium provident quibusdam quo quod ullam vel.</p>
   </div>
 </template>
 
