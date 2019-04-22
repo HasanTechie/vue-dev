a0e5a17099d4673982fdead1d90be43b82e64615
hasanabbax <hasanabbax@gmail.com>
2019-04-22 01:02:16 +0000

    added axios; added file for apiRequests in services

--- src/views/SelectCompetitor.vue
+++ src/views/SelectCompetitor.vue
@@ -1,20 +1,31 @@
 <template>
     <div>
-        <multiselect
-                v-model="selected"
-                :options="options">
+        <label class="typo__label">Simple select / dropdown</label>
+        <multiselect v-model="value" :options="options" :multiple="true" :close-on-select="false" :clear-on-select="false" :preserve-search="true" placeholder="Pick some" label="name" track-by="name" :preselect-first="true">
+            <template slot="selection" slot-scope="{ values, search, isOpen }"><span class="multiselect__single" v-if="values.length &amp;&amp; !isOpen">{{ values.length }} options selected</span></template>
         </multiselect>
+        <pre class="language-json"><code>{{ value  }}</code></pre>
     </div>
 </template>
 
 <script>
     import Multiselect from 'vue-multiselect'
+
     export default {
-        components: { Multiselect },
+        components: {
+            Multiselect
+        },
         data () {
             return {
-                selected: null,
-                options: ['list', 'of', 'options']
+                value: [],
+                options: [
+                    { name: 'Vue.js', language: 'JavaScript' },
+                    { name: 'Adonis', language: 'JavaScript' },
+                    { name: 'Rails', language: 'Ruby' },
+                    { name: 'Sinatra', language: 'Ruby' },
+                    { name: 'Laravel', language: 'PHP' },
+                    { name: 'Phoenix', language: 'Elixir' }
+                ]
             }
         }
     }
