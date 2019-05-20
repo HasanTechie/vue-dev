2c65fda5d37a43d7325e03527e8b6dc3c7420ca6
hasanabbax <hasanabbax@gmail.com>
2019-05-20 03:13:23 +0000

    added Login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,26 @@
 <template>
-    <div></div>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md4>
+                <v-card class="elevation-12">
+                    <v-toolbar dark color="primary">
+                        <v-toolbar-title>Login form</v-toolbar-title>
+                        <v-spacer></v-spacer>
+                    </v-toolbar>
+                    <v-card-text>
+                        <v-form>
+                            <v-text-field prepend-icon="email" name="email" label="Your Email" type="text"></v-text-field>
+                            <v-text-field id="password" prepend-icon="lock" name="Your Password" label="Password" type="password"></v-text-field>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions  class="pa-3">
+                        <v-spacer></v-spacer>
+                        <v-btn color="primary">Login</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
