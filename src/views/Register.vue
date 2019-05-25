f643ca67cbf4aa1dfbbd0f1740e2b0be343bc9d9
hasanabbax <hasanabbax@gmail.com>
2019-05-25 23:56:00 +0000

    working on validation

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -10,7 +10,12 @@
                     <v-card-text>
                         <v-form ref="form">
                             <v-text-field prepend-icon="person" v-model="name" label="Your Name"
-                                          type="text" :rules="inputRules"></v-text-field>
+                                          type="text" :error-messages="nameErrors"
+                                          :counter="10"
+                                          required
+                                          @input="$v.name.$touch()"
+                                          @blur="$v.name.$touch()"
+                            ></v-text-field>
                             <v-text-field prepend-icon="email" v-model="email" label="Your Email"
                                           type="text"></v-text-field>
                             <v-text-field id="password" prepend-icon="lock" v-model="password" label="Your Password"
@@ -52,6 +57,8 @@
 <script>
     import apiRequests from '@/services/apiRequests.js'
     import Multiselect from 'vue-multiselect'
+    import {validationMixin} from 'vuelidate'
+    import {required, maxLength, email} from 'vuelidate/lib/validators'
 
     export default {
         name: 'Register',
@@ -61,17 +68,30 @@
         props: {
             source: String
         },
+        mixins: [validationMixin],
+
+        validations: {
+            name: {required, maxLength: maxLength(10)},
+            email: {required, email},
+            select: {required},
+            checkbox: {
+                checked(val) {
+                    return val
+                }
+            }
+        },
         data() {
             return {
-                name: null,
+                name: '',
                 email: null,
                 password: null,
                 hotel_id: null,
                 inputRules: [
                     v => !!v || 'This field is required',
-                    v =>  v && v.length >= 3 || 'Minimum length is 3 characters'
+                    v => v && v.length >= 3 || 'Minimum length is 3 characters'
+
                 ],
-                
+                checkbox: false,
                 terms: false,
                 value: [],
                 options: [],
@@ -79,6 +99,34 @@
         },
         created() {
             this.getAllHotels()
+        },
+        computed: {
+            checkboxErrors() {
+                const errors = []
+                if (!this.$v.checkbox.$dirty) return errors
+                !this.$v.checkbox.checked && errors.push('You must agree to continue!')
+                return errors
+            },
+            selectErrors() {
+                const errors = []
+                if (!this.$v.select.$dirty) return errors
+                !this.$v.select.required && errors.push('Item is required')
+                return errors
+            },
+            nameErrors() {
+                const errors = []
+                if (!this.$v.name.$dirty) return errors
+                !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
+                !this.$v.name.required && errors.push('Name is required.')
+                return errors
+            },
+            emailErrors() {
+                const errors = []
+                if (!this.$v.email.$dirty) return errors
+                !this.$v.email.email && errors.push('Must be valid e-mail')
+                !this.$v.email.required && errors.push('E-mail is required')
+                return errors
+            }
         },
         methods: {
             nameWithCity({name, city}) {
@@ -98,7 +146,13 @@
                     })
             },
             register() {
-                if (this.$refs.form.validate()) {
+
+                this.$v.$touch()
+                if (this.$v.$invalid) {
+                    this.submitStatus = 'ERROR'
+                    console.log('reachedErorr')
+                } else {
+                    console.log('reached')
                     this.$store.dispatch('register', {
                         name: this.name,
                         email: this.email,
