472414ab973928c09b81b8f5cfc44689ce03181e
globalw <wilhelm.bender@gmx.de>
2019-06-05 13:23:07 +0200

    get=50 instead 0

--- src/services/apiRequests.js
+++ src/services/apiRequests.js
@@ -11,34 +11,34 @@ const apiClient = axios.create({
 
 export default {
     getHotels(city = 'All') {
-        return apiClient.get('/hotels&get=0' + this.getApiKey() + '&city=' + city)
+        return apiClient.get('/hotels&get=50' + this.getApiKey() + '&city=' + city)
     },
     getPrices() {
         return apiClient.get('roomsprices&get=40' + this.getApiKey() + '&hotelid=28&datefrom=2019-04-25&dateto=2019-12-25')
     },
     getCompetitorAvgPrices(competitorsids) {
-        return apiClient.get('competitorsavgprices&get=0' + this.getApiKey() + '&hotelid=21&datefrom=2019-04-25&dateto=2020-05-28&competitorsid=' + competitorsids)
+        return apiClient.get('competitorsavgprices&get=50' + this.getApiKey() + '&hotelid=21&datefrom=2019-04-25&dateto=2020-05-28&competitorsid=' + competitorsids)
     },
     getCompetitorAvgPricesFix(competitorsids='1024,3045,4106') {
-        return apiClient.get('competitorsavgprices&get=0' + this.getApiKey() + '&hotelid=21&datefrom=2019-04-25&dateto=2020-05-28&competitorsid=' + competitorsids)
+        return apiClient.get('competitorsavgprices&get=50' + this.getApiKey() + '&hotelid=21&datefrom=2019-04-25&dateto=2020-05-28&competitorsid=' + competitorsids)
     },
     getCompetitorAvgPricesForDates(competitorsids, dateOne, dateTwo) {
-        return apiClient.get('competitorsavgprices&get=0' + this.getApiKey() + '&hotelid=21&datefrom=' + dateOne + '&dateto=' + dateTwo + '&competitorsid=' + competitorsids)
+        return apiClient.get('competitorsavgprices&get=50' + this.getApiKey() + '&hotelid=21&datefrom=' + dateOne + '&dateto=' + dateTwo + '&competitorsid=' + competitorsids)
     },
     getCompetitorPricesApex(hotelid, competitorsids, selectedValue, dateOne = '2019-01-01', dateTwo = '2021-01-01') {
-        return apiClient.get('competitorspricesapex&get=0' + this.getApiKey() + '&hotelid=' + hotelid + '&datefrom=' + dateOne + '&dateto=' + dateTwo + '&competitorsid=' + competitorsids + '&room=' + selectedValue)
+        return apiClient.get('competitorspricesapex&get=50' + this.getApiKey() + '&hotelid=' + hotelid + '&datefrom=' + dateOne + '&dateto=' + dateTwo + '&competitorsid=' + competitorsids + '&room=' + selectedValue)
     },
     getCompetitorRoomsPrices(competitorsids='1024,3045,4106', hotelid='21') {
-        return apiClient.get('competitorsroomsprices&get=0' + this.getApiKey() + '&hotelid='+hotelid+'&datefrom=2019-04-25&dateto=2020-05-28&competitorsid=' + competitorsids)
+        return apiClient.get('competitorsroomsprices&get=50' + this.getApiKey() + '&hotelid='+hotelid+'&datefrom=2019-04-25&dateto=2020-05-28&competitorsid=' + competitorsids)
     },
     getCompetitorRoomsPricesFix(competitorsids='1024,3045,4106', hotelid='21') {
-        return apiClient.get('competitorsroomsprices&get=0' + this.getApiKey() + '&hotelid='+hotelid+'&datefrom=2019-04-25&dateto=2020-05-28&competitorsid=' + competitorsids)
+        return apiClient.get('competitorsroomsprices&get=50' + this.getApiKey() + '&hotelid='+hotelid+'&datefrom=2019-04-25&dateto=2020-05-28&competitorsid=' + competitorsids)
     },
     getCompetitors() {
         return '&competitorsid=' + this.$store.getters.competitorsids
     },
     getEvents(city = 'Berlin') {
-        return apiClient.get('events&get=0' + this.getApiKey() + '&city=' + city)
+        return apiClient.get('events&get=50' + this.getApiKey() + '&city=' + city)
     },
     getApiKey() {
         return '&apiKey=KuKMQbgZPv0PRC6GqCMlDQ7fgdamsVY75FrQvHfoIbw4gBaG5UX0wfk6dugKxrtW'
