0ad4656be76bf0bb1b35c4f5cd0f51e88f804020
hasanabbax <hasanabbax@gmail.com>
2019-07-15 13:42:15 +0000

    added template

--- src/views/Image.vue
+++ src/views/Image.vue
@@ -1,5 +1,41 @@
 <template>
-    <div></div>
+    <div>
+        <v-container>
+            <div class="uploader"
+                 @dragenter="OnDragEnter"
+                 @dragleave="OnDragLeave"
+                 @dragover.prevent
+                 @drop="onDrop"
+                 :class="{ dragging: isDragging }">
+
+                <div class="upload-control" v-show="images.length">
+                    <label for="file">Select a file</label>
+                    <button @click="upload">Upload</button>
+                </div>
+
+
+                <div v-show="!images.length">
+                    <i class="fa fa-cloud-upload"></i>
+                    <p>Drag your images here</p>
+                    <div>OR</div>
+                    <div class="file-input">
+                        <label for="file">Select a file</label>
+                        <input type="file" id="file" @change="onInputChange" multiple>
+                    </div>
+                </div>
+
+                <div class="images-preview" v-show="images.length">
+                    <div class="img-wrapper" v-for="(image, index) in images" :key="index">
+                        <img :src="image" :alt="`Image Uplaoder ${index}`">
+                        <div class="details">
+                            <span class="name" v-text="files[index].name"></span>
+                            <span class="size" v-text="getFileSize(files[index].size)"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </v-container>
+    </div>
 </template>
 
 <script>
