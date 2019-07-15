59702d1a43b9385cc605155cc97951d6a28354f8
hasanabbax <hasanabbax@gmail.com>
2019-07-15 13:45:22 +0000

    addition of file methods ; inputchange,file size, add image

--- src/views/Image.vue
+++ src/views/Image.vue
@@ -63,6 +63,38 @@
                 if (this.dragCount <= 0)
                     this.isDragging = false;
             },
+            onInputChange(e) {
+                const files = e.target.files;
+                Array.from(files).forEach(file => this.addImage(file));
+            },
+            onDrop(e) {
+                e.preventDefault();
+                e.stopPropagation();
+                this.isDragging = false;
+                const files = e.dataTransfer.files;
+                Array.from(files).forEach(file => this.addImage(file));
+            },
+            addImage(file) {
+                if (!file.type.match('image.*')) {
+                    this.$toastr.e(`${file.name} is not an image`);
+                    return;
+                }
+                this.files.push(file);
+                const img = new Image(),
+                    reader = new FileReader();
+                reader.onload = (e) => this.images.push(e.target.result);
+                reader.readAsDataURL(file);
+            },
+            getFileSize(size) {
+                const fSExt = ['Bytes', 'KB', 'MB', 'GB'];
+                let i = 0;
+
+                while(size > 900) {
+                    size /= 1024;
+                    i++;
+                }
+                return `${(Math.round(size * 100) / 100)} ${fSExt[i]}`;
+            },
         }
     }
 </script>
