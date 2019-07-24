bb18fec2989ff93d9169af7e298ddc1aa2002b36
hasanabbax <hasanabbax@gmail.com>
2019-07-24 20:14:20 +0000

    added CSV form; minor change in nameings

--- src/views/UploadCSV.vue
+++ src/views/UploadCSV.vue
@@ -1,13 +1,54 @@
 <template>
-    <div>
-        <v-container>
-        </v-container>
-    </div>
+    <v-container fluid grid-list-xl>
+        <v-layout wrap align-center>
+            <v-flex xs12 sm3 d-flex>
+
+                <v-file-input
+                        v-model="files"
+                        color="indigo accent-4"
+                        counter
+                        label="Upload CSV"
+                        multiple
+                        placeholder="Select your CSV"
+                        prepend-icon="mdi-paperclip"
+                        outlined
+                        :display-size="1000"
+                >
+                    <template v-slot:selection="{ index, text }">
+                        <v-chip
+                                v-if="index < 2"
+                                color="indigo accent-4"
+                                dark
+                                label
+                                small
+                        >
+                            {{ text }}
+                        </v-chip>
+
+                        <span
+                                v-else-if="index === 2"
+                                class="overline grey--text text--darken-3 mx-2"
+                        >
+        +{{ files.length - 2 }} File(s)
+      </span>
+                    </template>
+                </v-file-input>
+            </v-flex>
+            <v-flex xs12 sm3 d-flex>
+                <v-btn color="success" @click="uploadImages"><v-icon left>cloud_done</v-icon>Upload CSV</v-btn>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
     export default {
-        name: "CSV"
+        name: "UploadCSV",
+        data() {
+            return {
+                files: []
+            }
+        },
     }
 </script>
 
--- src/views/UploadImage.vue
+++ src/views/UploadImage.vue
@@ -43,7 +43,7 @@
 
 <script>
     export default {
-        name: "ImageUpload",
+        name: "UploadImage",
         data() {
             return {
                 files: []
