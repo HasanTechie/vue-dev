bf3b1f1460fd1ce601ecf731ce87697f25361d40
Wilhelm Bender <wilhelm.bender@globosoft.de>
2019-07-30 12:23:26 +0200

    working on UploadCSV

---
+++ src/components/OptimalPriceTable.vue
@@ -0,0 +1,209 @@
+<template>
+<div>
+  <div class="container">
+    <div class="panel panel-sm">
+      <div class="panel-heading"> 
+        <h4>Optimal Price List, Last Update: 10/08/2019 </h4>
+      </div>
+      <div class="panel-body">
+        <div class="form-group">
+        <v-data-table v-if="parse_csv"
+            :headers="mockheaders"
+            :items="mockitems"
+            :items-per-page="10"
+            item-key="name"
+            class="elevation-1"
+            :footer-props="{
+                showFirstLastPage: true,
+                firstIcon: 'mdi-arrow-collapse-left',
+                lastIcon: 'mdi-arrow-collapse-right',
+                prevIcon: 'mdi-minus',
+                nextIcon: 'mdi-plus'
+            }"
+        ></v-data-table>    
+        </div>    
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "CSVImport",
+        data() {
+            return {
+                channel_name: '',
+                channel_fields: [],
+                channel_entries: [],
+                parse_header: [],
+                parse_csv: [],
+                sortOrders:{},
+                sortKey: '',
+                mockheaders: [
+                    {
+                        text: 'Date',
+                        align: 'left',
+                        sortable: false,
+                        value: 'date',
+                    },
+                    { text: 'Room Categroy', value: 'category' },
+                    { text: 'Optimal Price', value: 'optprice' },
+                ],
+                mockitems: [
+                    {
+                        date: '11/08/2019',
+                        category: 'Superior Room',
+                        optprice: 80.9,
+                    },
+                    {
+                        date: '12/084/2019',
+                        category: 'Superior Room',
+                        optprice: 55.1,
+                    },
+                    {
+                        date: '13/08/2019',
+                        category: 'Superior Room',
+                        optprice: 64.5,
+                    },
+                    {
+                        date: '14/08/2019',
+                        category: 'Superior Room',
+                        optprice: 60.8,
+                    },
+                    {
+                        date: '15/08/2019',
+                        category: 'Superior Room',
+                        optprice: 60.8,
+                    },
+                    {
+                        date: '16/08/2019',
+                        category: 'Superior Room',
+                        optprice: 80.6,
+                    },
+                    {
+                        date: '17/084/2019',
+                        category: 'Superior Room',
+                        optprice: 81.4,
+                    },
+                    {
+                        date: '18/08/2019',
+                        category: 'Superior Room',
+                        optprice: 84.3,
+                    },
+                    {
+                        date: '19/08/2019',
+                        category: 'Superior Room',
+                        optprice: 66.9,
+                    },
+                    {
+                        date: '20/08/2019',
+                        category: 'Superior Room',
+                        optprice: 50.6,
+                    },
+                ],
+            }
+        },
+        filters: {
+            capitalize: function (str) {
+            return str.charAt(0).toUpperCase() + str.slice(1)
+            }
+        },
+        methods: {
+            sortBy: function (key) {
+                var vm = this
+                vm.sortKey = key
+                vm.sortOrders[key] = vm.sortOrders[key] * -1
+            },
+            csvJSON(csv){
+                var vm = this
+                var lines = csv.split("\n")
+                var result = []
+                var headers = lines[0].split(",")
+                vm.parse_header = lines[0].split(",") 
+                lines[0].split(",").forEach(function (key) {
+                    vm.sortOrders[key] = 1
+            })
+            
+            lines.map(function(line, indexLine){
+                if (indexLine < 1) return // Jump header line
+                
+                var obj = {}
+                var currentline = line.split(",")
+                
+                headers.map(function(header, indexHeader){
+                obj[header] = currentline[indexHeader]
+                })
+                
+                result.push(obj)
+            })
+            
+            result.pop() // remove the last item because undefined values
+            
+            return result // JavaScript object
+            },
+            loadCSV(e) {
+                var vm = this
+                if (window.FileReader) {
+                    var reader = new FileReader()
+                    reader.readAsText(e.target.files[0])
+                    // Handle errors load
+                    reader.onload = function(event) {
+                        var csv = event.target.result
+                        vm.parse_csv = vm.csvJSON(csv)
+                    }
+                    reader.onerror = function(evt) {
+                        if(evt.target.error.name == "NotReadableError") {
+                            alert("Can not read file !")
+                        }
+                    }
+                } else {
+                    alert('FileReader are not supported in this browser.')
+                }
+            }
+        }
+    }
+</script>
+<style scoped>
+    html, body {
+        margin: 0;
+        padding: 0;
+    }
+    body {
+        margin: 32px auto;
+    }
+    .panel {
+        border: 2px solid #dfdfdf;
+        box-shadow: rgba(0, 0, 0, 0.15) 0 1px 0 0;
+        margin: 10px;
+    } 
+    .panel.panel-sm {
+        max-width: 700px;
+        margin: 10px auto;
+    }
+    .panel-heading {
+        border-bottom: 2px solid #dfdfdf;
+    }
+        .panel-heading h1, .panel-heading h2, .panel-heading h3, .panel-heading h4, .panel-heading h5, .panel-heading h6 {
+        margin: 0;
+        padding: 0;
+    }
+        .panel-body .checkbox-inline {
+        padding: 15px 20px;
+    }
+    table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+    }
+
+    tr:nth-child(even) {
+        background-color: #dddddd;
+    }
+</style>
--- src/views/UploadCSV.vue
+++ src/views/UploadCSV.vue
@@ -41,12 +41,98 @@
                 </v-btn>
             </v-flex>
         </v-layout>
+        <v-layout wrap align-center>
+            <VueCsvImport/>
+        </v-layout>
     </v-container>
 </template>
 
 <script>
+    import VueCsvImport from '@/components/VueCsvImport.vue'
     export default {
         name: "UploadCSV",
+        components: [
+            VueCsvImport
+        ],
+        data() {
+            return {
+                files: [],
+            }
+        },
+        methods: {
+            uploadCSVs() {
+
+                if (this.files.length) {
+
+                    var form = new FormData
+
+                    for (let i = 0; i < this.files.length; i++) {
+                        form.append('CSVs[]', this.files[i])
+                    }
+
+                    this.files = [];
+                    this.$store.dispatch('uploadCSVs', form).then(() => {
+                    })
+                }
+            }
+        }
+    }
+</script>
+<template>
+    <v-container fluid grid-list-xl>
+        <v-layout wrap align-center>
+            <v-flex xs12 sm3 d-flex>
+
+                <v-file-input
+                        v-model="files"
+                        color="indigo accent-4"
+                        counter
+                        label="Upload CSVs"
+                        multiple
+                        placeholder="Select your CSVs"
+                        prepend-icon="mdi-paperclip"
+                        outlined
+                        :display-size="1000"
+                >
+                    <template v-slot:selection="{ index, text }">
+                        <v-chip
+                                v-if="index < 4"
+                                color="indigo accent-4"
+                                dark
+                                label
+                                small
+                        >
+                            {{ text }}
+                        </v-chip>
+
+                        <span
+                                v-else-if="index === 4"
+                                class="overline grey--text text--darken-3 mx-2"
+                        >
+                        +{{ files.length - 4 }} File(s)
+                        </span>
+                    </template>
+                </v-file-input>
+            </v-flex>
+            <v-flex xs12 sm3 d-flex>
+                <v-btn color="success" @click="uploadCSVs">
+                    <v-icon left>cloud_done</v-icon>
+                    Upload CSVs
+                </v-btn>
+            </v-flex>
+        </v-layout>
+        <OptimalPriceTable/>
+        </v-container>
+
+</template>
+
+<script>
+    import OptimalPriceTable from '@/components/OptimalPriceTable.vue'
+        export default {
+        name: "UploadCSV",
+        components:{
+            OptimalPriceTable
+        },
         data() {
             return {
                 files: [],
@@ -72,6 +158,9 @@
     }
 </script>
 
+<style scoped>
+
+</style>
 <style scoped>
 
 </style>
