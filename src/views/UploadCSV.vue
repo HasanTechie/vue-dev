92c8f217d643f3941b7937eb9ad84b6ceab62a96
Wilhelm Bender <wilhelm.bender@globosoft.de>
2019-07-31 10:26:35 +0200

    nicer upload csv window

--- src/views/UploadCSV.vue
+++ src/views/UploadCSV.vue
@@ -1,94 +1,86 @@
 <template>
-    <v-container fluid grid-list-xl>
-        <v-layout wrap align-center>
-            <v-flex xs12 
-                    sm12
-                    lg12 
-                    d-flex 
-                    class="pa-3 mb-3 feature-pane"
-            >
-
-                <v-file-input
-                        v-model="files"
-                        color="indigo accent-4"
-                        counter
-                        label="Upload CSVs"
-                        multiple
-                        placeholder="Select your CSVs"
-                        prepend-icon="mdi-paperclip"
-                        outlined
-                        :display-size="1000"
-                >
-                    <template v-slot:selection="{ index, text }">
-                        <v-chip
-                                v-if="index < 4"
-                                color="indigo accent-4"
-                                dark
-                                label
-                                small
-                        >
-                            {{ text }}
-                        </v-chip>
-
-                        <span
-                                v-else-if="index === 4"
-                                class="overline grey--text text--darken-3 mx-2"
-                        >
-                        +{{ files.length - 4 }} File(s)
-                        </span>
-                    </template>
-                </v-file-input>
+    <v-layout row wrap >
             
-                <v-btn color="success" @click="uploadCSVs">
-                    <v-icon left>cloud_done</v-icon>
-                    Upload CSVs
-                </v-btn>
-            </v-flex>
-        </v-layout>
-        <v-layout row wrap="">
-            <v-flex
-                    sm12
-                    lg12
-                    class="pa-3 mb-3 feature-pane"
-            >
-                <v-card color="primary">
-                    <v-menu 
-                        v-model="currentcapacity"
-                    >
-                        <template v-slot:activator="{ on }">
-                            <v-btn
-                            color="primary"
-                            dark
-                            v-on="on"
+            <v-flex d-flex 
+                    xs12 
+                    sm12 
+                    md3
+                    lg3 class="pa-3 mb-3 feature-pane">
+                <v-container fluid grid-list-xl>
+                    <v-layout wrap>
+                        <v-flex d-flex >
+
+                            <v-file-input
+                            v-model="files"
+                            color="indigo accent-4"
+                            counter
+                            label="Upload Occupancy"
+                            multiple
+                            placeholder="Select Your Occupancy Sheet"
+                            prepend-icon="mdi-paperclip"
+                            outlined
+                            :display-size="1000"
                             >
-                            Select Current Capacity
+                                <template v-slot:selection="{ index, text }">
+                                    <v-chip
+                                            v-if="index < 4"
+                                            color="indigo accent-4"
+                                            dark
+                                            label
+                                            small
+                                    >
+                                        {{ text }}
+                                    </v-chip>
+
+                                    <span
+                                            v-else-if="index === 4"
+                                            class="overline grey--text text--darken-3 mx-2"
+                                    >
+                                    +{{ files.length - 4 }} File(s)
+                                    </span>
+                                </teMplate>
+                            </v-file-input>
+
+                        </v-flex>
+                        
+                        <v-flex xs12 sm12 md12 lg12 d-flex>
+                            <v-divider></v-divider>
+                        </v-flex>
+
+                        <v-flex xs12 sm12 md12 align-center="true">
+                            <v-btn color="success" @click="uploadCSVs">
+                                <v-icon left>cloud_done</v-icon>
+                                Upload
                             </v-btn>
-                        </template>
-                        <v-list>
-                            <v-list-item
-                            v-for="(item, index) in capacities"
-                            :key="index"
-                            @click="showCSV"
-                            >
-                                <v-list-item-title>{{ item.capacity }}</v-list-item-title>
-                            </v-list-item>
-                        </v-list>
-                    </v-menu>
-                </v-card>
+                        </v-flex>
+                        <v-flex xs12 sm12 md12 d-flex>
+                            <v-divider></v-divider>
+                        </v-flex>
+                        <v-flex d-flex xs12 sm12 md12>
+                            <v-select
+                                :items="capacities"
+                                v-model="currentcapacity"
+                                label="Select Capacity"
+                                v-on:close="showCSV"
+                            ></v-select>
+                            
+                        </v-flex>
+                        <v-flex xs12 sm12 md12 d-flex>
+                            <v-divider ></v-divider>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+
             </v-flex>
-            <v-flex
-                    sm12
-                    lg12
-                    class="pa-3 mb-3 feature-pane"
-            ><v-data-table
-                :headers="headers"
-                :items="ratedata"
-                :items-per-page="5"
-                class="elevation-1"
-            ></v-data-table>
+            <v-flex xs12 sm12 md9 lg9 class="pa-3 mb-3 feature-pane">
+                <v-data-table
+                    :headers="headers"
+                    :items="ratedata"
+                    :items-per-page="5"
+                    class="elevation-1"
+                ></v-data-table>
             </v-flex>
-        </v-layout>
-    </v-container>
+    </v-layout>
 </template>
 
 <script>
@@ -153,14 +145,15 @@
                 console.log('hello, you clicked me')
             },
             showCSV(){
-                console.log('userid :' + this.userid)
+                //console.log('userid :' + this.userid)
 
                 apiRequests.getProcessedCSVs(this.userid)
                 .then(response => {
                     //console.log(response.data)
-                    var responseDataArray = Object.keys(response.data.data).map((key) => {
-                                    return response.data.data[key]
-                    })
+                    var responseDataArray = Object.keys(
+                                                response.data.data).map((key) => {
+                                                    return response.data.data[key]
+                                                })
                     console.log(String(responseDataArray[11]['file_url']))
                     
                     var request = require("request");
@@ -182,8 +175,13 @@
                         for (let i=0; i<jsonObj.length; i+=1) {
                             let obj = {}
                             obj.date = (jsonObj[i].field1),
-                            obj.optprice = Number(jsonObj[i][this.currentcapacity][0]).toFixed(2)
-                            console.log('jsonObj[0]: '+jsonObj[0][this.currentcapacity][0])
+
+                            obj.optprice =  Number(
+                                                jsonObj[i][this.currentcapacity][0]
+                                            ).toFixed(2)
+
+                            console.log('jsonObj[0]: '+
+                                jsonObj[0][this.currentcapacity][0])
 
                             ratedata.push(obj)
                         }
@@ -200,9 +198,10 @@
                 apiRequests.getProcessedCSVs(this.userid)
                 .then(response => {
                     console.log(response.data)
-                    var responseDataArray = Object.keys(response.data.data).map((key) => {
-                                    return response.data.data[key]
-                    })
+                    var responseDataArray = Object.keys(
+                                                response.data.data).map((key) => {
+                                                    return response.data.data[key]
+                                                })
                     console.log(String(responseDataArray[11]['file_url']))
                     
                     var request = require("request");
@@ -226,15 +225,14 @@
                         // get all possible capacities
                         let capacities = []
                         for (var key in jsonObj[0]) {
-                            let obj = {}
-                            obj.capacity = (key)
-                            capacities.push(obj)
+                            capacities.push(key)
                         }
-                        //console.log(capacities[0].capacity)
+                        //console.log(capacities.capacity)
                         // the last entry is the date...
-                        capacities.pop()
                         this.capacities = capacities
-                        this.currentcapacity = this.capacities[0].capacity
+                        console.log("this.capacities[0]"+this.capacities[0])
+
+                        this.currentcapacity = this.capacities[0]
                     })
                 })
                 .catch(error => {
@@ -262,47 +260,5 @@
 </script>
 
 <style>
-#csvtable {
-        font-family: "Avenir", Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-    .container {
-        text-align: left;
-    }
-    pre code {
-        background-color: #eee;
-        border: 1px solid #999;
-        display: block;
-        padding: 20px;
-    }
-    #app .form {
-        text-align: left;
-    }
-    .btn {
-        display: inline-block;
-        font-weight: 400;
-        text-align: center;
-        white-space: nowrap;
-        vertical-align: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-        border: 1px solid transparent;
-        padding: .375rem .75rem;
-        font-size: 1rem;
-        line-height: 1.5;
-        border-radius: .25rem;
-        transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-    }
 
-    .btn-primary {
-        color: #fff;
-        background-color: #007bff;
-        border-color: #007bff;
-    }
 </style>
