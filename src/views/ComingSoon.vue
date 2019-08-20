4bef1797d0cb3f9e270ba3c38f490299339d190f
hasanabbax <hasanabbax@gmail.com>
2019-08-20 13:48:29 +0000

    minor edit

--- src/views/ComingSoon.vue
+++ src/views/ComingSoon.vue
@@ -1,7 +1,7 @@
 <template>
 
     <div>
-        <h2 class="display-3 my-4">Section under construction</h2>
+        <h2 class="display-2 my-4">Section under construction</h2>
         <p>This Section is currently under construction and will be available soon.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab commodi cupiditate inventore minima nesciunt nulla quis similique sunt. Culpa cum dolorem esse harum illum iure neque, nesciunt praesentium quas vero.</p>
     </div>
